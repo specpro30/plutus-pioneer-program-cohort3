b4253b05fceef0ee2fd70da39cffd0ec22463167
Justin Tsang <specrpro30@gmail.com>
2022-01-21 00:11:31 +0800

    completed notes for Gift.hs

--- week02/src/Week02/Gift.hs
+++ week02/src/Week02/Gift.hs
@@ -30,47 +30,49 @@ import           Text.Printf         (printf)
 
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
-{-# INLINABLE mkValidator #-}
-mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-mkValidator _ _ _ = ()
+{-# INLINABLE mkValidator #-}  --Haskell pragma to allow the compiler to inline the definition of mkValidator in the || brackets
+mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()   --Datum, Redeemer and Context data type is BuiltinData.
+                                                                 -- "()" Unit is the return type, similar to void
+mkValidator _ _ _ = ()                                           --always passes validation. 
 
 validator :: Validator
-validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
-
-valHash :: Ledger.ValidatorHash
+validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])  --the oxford brackets "||" converts a Haskell expression to a plutus core syntax tree of that expression
+                                                                        --"$$" splices the syntax tree into source code at that point. The result is like hardcoding plutus core at that point in code
+                                                                        --the mkValidatorScript then turns it into a validator
+valHash :: Ledger.ValidatorHash            --Creates a hash of the validator
 valHash = Scripts.validatorHash validator
 
-scrAddress :: Ledger.Address
+scrAddress :: Ledger.Address               --turns validator into an address on the blockchain
 scrAddress = scriptAddress validator
 
 type GiftSchema =
-            Endpoint "give" Integer
-        .\/ Endpoint "grab" ()
+            Endpoint "give" Integer        --end points allow user to trigger something. "give" will send Lovelaces
+        .\/ Endpoint "grab" ()             --"grab" will spend Lovelaces
 
 give :: AsContractError e => Integer -> Contract w s e ()
 give amount = do
-    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount
-    ledgerTx <- submitTx tx
+    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI 0) $ Ada.lovelaceValueOf amount --The datum is just an arbitrary one. Will pay the amount to the valHash
+    ledgerTx <- submitTx tx                                                                     --This line will wait for confirmation
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-    logInfo @String $ printf "made a gift of %d lovelace" amount
+    logInfo @String $ printf "made a gift of %d lovelace" amount                                --Logs to playground
 
 grab :: forall w s e. AsContractError e => Contract w s e ()
 grab = do
-    utxos <- utxosAt scrAddress
-    let orefs   = fst <$> Map.toList utxos
-        lookups = Constraints.unspentOutputs utxos      <>
-                  Constraints.otherScript validator
+    utxos <- utxosAt scrAddress                                --finds all utxo sitting at that address
+    let orefs   = fst <$> Map.toList utxos                     --gets all the references to these utxos
+        lookups = Constraints.unspentOutputs utxos      <>     --tell wallet where to find utxos
+                  Constraints.otherScript validator            --spending transactio has to provide the validator in the transaction
         tx :: TxConstraints Void Void
-        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI 17 | oref <- orefs]
-    ledgerTx <- submitTxConstraintsWith @Void lookups tx
-    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI 17 | oref <- orefs]  --list of constraints for each utxo at give address. the tx being constructed must spend the utxo
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx                                             --helps wallet to construct the tx like how to find the utxos etc
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx                                                --logs message
     logInfo @String $ "collected gifts"
 
 endpoints :: Contract () GiftSchema Text ()
 endpoints = awaitPromise (give' `select` grab') >> endpoints
   where
-    give' = endpoint @"give" give
-    grab' = endpoint @"grab" $ const grab
+    give' = endpoint @"give" give          --blocks and waits for user to provide an integer
+    grab' = endpoint @"grab" $ const grab  --blocks until the user unblocks
 
 mkSchemaDefinitions ''GiftSchema
 
